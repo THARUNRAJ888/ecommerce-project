72258f401df3297795b7676e6b77dabdda436d96
THARUNRAJ888 <tharunraj.good22@gmail.com>
2025-11-10 21:34:48 +0530

    Initial e-commerce project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,9 +8,7 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '/api')
-      }
-    }
-  }
+      },
+    },
+  },
 })
